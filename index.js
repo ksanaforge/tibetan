651d790073839e9f19b9414f26f4eca49b24e1b4
yapcheahshen <yapcheahshen@gmail.com>
2016-01-06 20:37:20 +0800

    add to index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var tibetDoc=require("./parse.js");
 var wylie=require("./wylie");
-module.exports={tibetDoc:tibetDoc,wylie:wylie}
+var abridge=require("./abridge");
+module.exports={tibetDoc:tibetDoc,wylie:wylie,abridge:abridge}
